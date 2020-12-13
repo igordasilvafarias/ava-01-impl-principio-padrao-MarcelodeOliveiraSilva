74539a3cdc02f2780560acd929bb5503bb5b9826
igordasilvafarias <igordasilvafarias@gmail>
2020-12-13 14:59:00 -0300

    Funcionalidade do Igor

---
+++ src/Function-Igor.ts
@@ -0,0 +1,9 @@
+// tslint:disable-next-line: no-shadowed-variable
+function ola(who: string = "World") {
+  console.log(`Hello ${who}`);
+
+}
+
+const who: string = "Marcelo de Oliveira Silva"
+
+ola(who)
